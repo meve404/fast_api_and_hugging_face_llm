541f4802445dc116c9759bf08539418b6c545957
meve404 <procontact325@gmail.com>
2026-06-25 19:42:06 -0400

    getting answers from HF

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
+from huggingface_hub import InferenceClient
 
-class Item(BaseModel):
-    name:str
-    description:str | None = None
-    price:float
-    tax:float | None = None
+load_dotenv()
 
+client=InferenceClient(api_key=os.environ.get('HF_TOKEN'))
+
+class MessageStructure(BaseModel):
+    role:str
+    content:str
 
 app = FastAPI()
 
-@app.post("/items/")
-async def create_item(item:Item):
-    return item
+@app.post("/prompt/")
+async def create_message(message:MessageStructure):
+    
+    promt = [
+        {
+            "role":message.role,
+            "content":message.content
+        }
+    ]
+
+    request = client.chat.completions.create(
+        model="meta-llama/Llama-3.1-8B-Instruct",
+        messages=promt,
+        max_tokens=500
+    )
+
+    return {'asnwer':request.choices[0].message.content}
